8428d57f383ca3a5b55b8847b02f7d99f8e39d8c
Alpagino715 <107158675+Alpagino715@users.noreply.github.com>
2022-08-24 22:48:55 -0500

    Add files via upload
    Final Schema/Analysis for Project

---
+++ SQL_Challenge_Analysis.sql
@@ -0,0 +1,57 @@
+﻿-- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT emp.emp_no,emp.last_name,emp.first_name,emp.sex,sal.salary
+FROM "Employees" AS emp
+INNER JOIN "Salaries" AS sal 
+ON sal.emp_no = emp.emp_no;
+
+-- 2. List first name, last name, and hire date for employees who were hired in 1986.
+SELECT emp.first_name, emp.last_name, emp.hire_date
+FROM "Employees" AS emp
+WHERE hire_date BETWEEN '1/1/1986' AND '12/31/1986'
+ORDER BY hire_date;
+
+-- 3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+SELECT dep.dept_no, dep.dept_name, dep_mgr.emp_no, emp.last_name, emp.first_name
+FROM "Departments" AS dep
+	JOIN "Dept_Mgr" AS dep_mgr
+	ON (dep.dept_no=dep_mgr.dept_no)
+		JOIN "Employees" AS emp
+		ON (dep_mgr.emp_no=emp.emp_no);
+        
+-- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT emp.emp_no, emp.last_name, emp.first_name, dep.dept_name
+FROM "Employees" AS emp
+    JOIN "Dept_Emp" AS dep_emp
+    ON (emp.emp_no=dep_emp.emp_no)
+        JOIN "Departments" as dep
+        ON (dep_emp.dept_no=dep.dept_no);
+        
+-- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT first_name, last_name, sex
+FROM "Employees"
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+-- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT emp.emp_no, emp.last_name, emp.first_name, dep.dept_name
+FROM "Employees" AS emp
+	JOIN "Dept_Emp" AS dep_emp
+	ON (emp.emp_no=dep_emp.emp_no)
+		JOIN "Departments" as dep
+		ON (dep_emp.dept_no=dep.dept_no)
+		WHERE dep.dept_name = 'Sales';
+
+-- 7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT emp.emp_no, emp.last_name, emp.first_name, dep.dept_name
+FROM "Employees" AS emp
+	JOIN "Dept_Emp" AS dep_emp
+	ON (emp.emp_no=dep_emp.emp_no)
+		JOIN "Departments" AS dep
+		ON (dep_emp.dept_no=dep.dept_no)
+		WHERE dep.dept_name = 'Sales' OR dep.dept_name = 'Development';
+
+-- 8. List the frequency count of employee last names (i.e., how many employees share each last name) in descending order.
+SELECT last_name, 
+COUNT(last_name) AS Frequency
+FROM "Employees"
+GROUP BY last_name
+ORDER BY Frequency DESC;
---
+++ SQL_Challenge_Schema.sql
@@ -0,0 +1,70 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/TRvU68
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+CREATE TABLE "Dept_Mgr" (
+    "dept_no" VARCHAR(10)   NOT NULL,
+    "emp_no" INT   NOT NULL
+);
+
+CREATE TABLE "Dept_Emp" (
+    "emp_no" INT   NOT NULL,
+    "dept_no" VARCHAR(10)   NOT NULL
+);
+
+CREATE TABLE "Titles" (
+    "title_id" VARCHAR(10)   NOT NULL,
+    "title" VARCHAR(30)   NOT NULL,
+    CONSTRAINT "pk_Titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+CREATE TABLE "Salaries" (
+    "emp_no" INT   NOT NULL,
+    "salary" INT   NOT NULL
+);
+
+CREATE TABLE "Employees" (
+    "emp_no" INT   NOT NULL,
+    "emp_title_id" VARCHAR(10)   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR(50)   NOT NULL,
+    "last_name" VARCHAR(50)   NOT NULL,
+    "sex" VARCHAR(1)   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    CONSTRAINT "pk_Employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "Departments" (
+    "dept_no" VARCHAR(10)   NOT NULL,
+    "dept_name" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_Departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+ALTER TABLE "Dept_Mgr" ADD CONSTRAINT "fk_Dept_Mgr_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Dept_Mgr" ADD CONSTRAINT "fk_Dept_Mgr_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Dept_Emp" ADD CONSTRAINT "fk_Dept_Emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Dept_Emp" ADD CONSTRAINT "fk_Dept_Emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "Titles" ("title_id");
+
